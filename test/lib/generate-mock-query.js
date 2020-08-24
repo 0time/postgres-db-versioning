6bf12cbe017608c22d9451625f43d59fd6e72a85
Henry Brown <h@0ti.me>
2020-08-24 17:14:34 -0500

    Add comments and simplification for the generate-mock-query test utility

--- test/lib/generate-mock-query.js
+++ test/lib/generate-mock-query.js
@@ -22,14 +22,19 @@ module.exports = function() {
       if (acc) return acc;
 
       if (isString(each.query)) {
+        // If the expected query is a string, expect an exact match for the actual query string,
+        // and return the matching response if the strings match exactly.
         if (query === each.query) {
           return each.response;
         }
-      } else if (each.query.test) {
+      } else if (_.isRegExp(each.query)) {
+        // If the expected query is a regular expression, use it to test the actual query string,
+        // and return the matching response if a match is found on the query string.
         if (each.query.test(query)) {
           return each.response;
         }
       } else {
+        // If the expected query is anything else, throw an error.
         throw new Error('invalid query expectation');
       }
 
